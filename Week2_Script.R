63b7f45a08db49cc06ea71f3b6eabb242d29cff2
AJM <61994052+ajm1313@users.noreply.github.com>
2020-04-02 07:56:08 +0000

    Add files via upload

---
+++ Week2_Script.R
@@ -0,0 +1,23 @@
+#Doing basic mathematical computations
+1+3
+1-3
+1*5
+1/6
+tan(2)
+sin(90)
+mean(c(1,2,3,4,5,6))
+sd(c(1, 2, 3, 4, 5, 6))
+plot(c(1,2,3,4,5,6), c(9,8,7,6,5,4))
+
+# Assignment operators in R 
+q<- 99
+w=99
+"Hello World"->e
+
+#Example of data types
+A<- c(1:10)
+B<- c(2,4,7,9,11,15,17,30)
+C<- c("male", "male“ , “female”, “male”, “female")
+D<- c(TRUE, FALSE,FALSE,TRUE,FALSE,TRUE)
+
+
